fafe2326c4093cad1035bcc846f12dfd44800933
Lavanyakutala <kutalalavanya79@gmail.com>
2018-02-20 12:11:50 +0530

    Revert "Modified a bit in class."
    This reverts commit 0d97e551308d271c9c779267bd23d65c70d13118.

--- Basics/src/AccessModifiers/NewClass.java
+++ Basics/src/AccessModifiers/NewClass.java
@@ -3,8 +3,8 @@ package AccessModifiers;
 public class NewClass {
 
 	public static void main(String[] args) {
-		int a,b;
-		System.out.println("hi");
+		// TODO Auto-generated method stub
+
 	}
 
 }
